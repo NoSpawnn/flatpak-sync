cab70108fa422db0107833a8897a280f706bb1d8
NoSpawnn <adam.blighe@protonmail.com>
2025-05-23 01:16:59 +0100

    SSH key generation and copy

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::{error::Error, process::Command};
 
+use clap::Parser;
+
 #[derive(Debug)]
 struct Flatpak {
     name: String,
@@ -39,9 +41,78 @@ impl InstallType {
     }
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let flatpaks = get_installed_flatpaks()?;
+/// Sync flatpaks between your devices!
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct SshInfo {
+    /// SSH username
+    #[arg(short, long)]
+    user: String,
 
+    /// SSH remote host
+    #[arg(short, long)]
+    remote: String,
+
+    /// SSH remote port
+    #[arg(long, default_value_t = 22)]
+    port: u16,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    env_logger::init();
+    let args = SshInfo::parse();
+
+    // Generate SSH keypair for new remote host
+    let hostname = String::from_utf8(Command::new("hostname").output()?.stdout)?;
+    let keypair_name = format!("{}_sync-key", &args.remote);
+    let output = Command::new("ssh-keygen")
+        .args([
+            "-t",
+            "rsa",
+            "-N",
+            "",
+            "-f",
+            &keypair_name,
+            "-C",
+            &format!("flatpak-sync@{hostname}"),
+        ])
+        .output()?;
+
+    // `ssh-copy-id` to that host (which prompts for password)
+    let output = Command::new("ssh-copy-id")
+        .args([
+            "-i",
+            &keypair_name,
+            "-p",
+            &args.port.to_string(),
+            &format!("{}@{}", &args.user, &args.remote),
+        ])
+        .status()?;
+
+    Ok(())
+}
+
+fn get_installed_flatpaks() -> Result<Vec<Flatpak>, Box<dyn Error>> {
+    let output = Command::new("flatpak")
+        .args(["list", "--app", "--columns=application,options"])
+        .output()?
+        .stdout;
+
+    let flatpaks = String::from_utf8(output)?
+        .lines()
+        .flat_map(|line| {
+            let mut parts = line.split('\t');
+            match (parts.next(), parts.next()) {
+                (Some(name), Some(options)) => Some(Flatpak::new(name, options)),
+                _ => None,
+            }
+        })
+        .collect();
+
+    Ok(flatpaks)
+}
+
+fn install_flatpaks_on_host(flatpaks: &[Flatpak], host: String) -> Result<(), Box<dyn Error>> {
     for f in flatpaks {
         println!("Installing {}", &f.name);
 
@@ -63,26 +134,4 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
-}
-
-fn get_installed_flatpaks() -> Result<Vec<Flatpak>, Box<dyn Error>> {
-    let output = Command::new("flatpak")
-        .arg("list")
-        .arg("--app")
-        .arg("--columns=application,options")
-        .output()?
-        .stdout;
-
-    let flatpaks = String::from_utf8(output)?
-        .lines()
-        .flat_map(|line| {
-            let mut parts = line.split('\t');
-            match (parts.next(), parts.next()) {
-                (Some(name), Some(options)) => Some(Flatpak::new(name, options)),
-                _ => None,
-            }
-        })
-        .collect();
-
-    Ok(flatpaks)
 }
